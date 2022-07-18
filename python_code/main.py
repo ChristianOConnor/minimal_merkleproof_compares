5a93b824242b10a9c214bc275a25ebff2ccbd124
ChristianOConnor <warpanomaly@gmail.com>
2022-07-18 18:13:00 -0400

    organize python output a little better

--- python_code/main.py
+++ python_code/main.py
@@ -1,32 +1,24 @@
-from itertools import tee
-from web3 import Web3
-from eth_utils import keccak
 from merkletools_instance import MerkleTools
 
 
 def merkle_run(hashtocheck):
     data = []
+    nmt = MerkleTools(hash_type="keccak")
     with open('address_list.txt', mode='r', encoding="utf-8") as in_file:
-        for line_in_file in in_file:
-            data.append(line_in_file.strip())
-        
-    tree = MerkleTools(hash_type="keccak")
-    tree.add_leaf(data, True)
-    tree.make_tree()
-    tree.is_ready
-    root_value = tree.get_merkle_root()
-
+        for address in in_file:
+            nmt.add_leaf(address.strip(), True)
+            data.append(address.strip())
     index_of_target = data.index(hashtocheck)
-    target_hash = tree.get_leaf(index_of_target)
-    proof = tree.get_proof(index_of_target)
-    print('this is the merkle root hash: ' + root_value)
-    print('\n')
+    nmt.make_tree()
+    root_value = nmt.get_merkle_root()
+    proof = nmt.get_proof(index_of_target)
+    target_hash = nmt.get_leaf(index_of_target)
 
-    for tr in range(tree.get_leaf_count()):
-        print(tree.get_leaf(tr))
-
-    # you can also pass in the hash value of 'hashtocheck', it will hash automatically if the user forgot to hash it
-    if tree.validate_proof(proof, target_hash, root_value):
+    for tr in range(nmt.get_leaf_count()):
+        print(nmt.get_leaf(tr))
+    
+    print('\n' + 'this is the merkle root: ' + root_value + '\n')
+    if nmt.validate_proof(proof, target_hash, root_value):
         print('address is in the tree')
     else:
         exit('address is not in the tree')
